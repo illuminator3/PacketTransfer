a243c280f79cc50a7aa9fc915f0d4002def8260a
illuminator3 <48388251+illuminator3@users.noreply.github.com>
2020-08-20 21:23:24 +0200

    Add packet queueing

--- src/main/java/me/illuminator3/packettransfer/packet/pre/PrePacketHandler.java
+++ src/main/java/me/illuminator3/packettransfer/packet/pre/PrePacketHandler.java
@@ -3,10 +3,19 @@ package me.illuminator3.packettransfer.packet.pre;
 import me.illuminator3.packettransfer.packet.Packet;
 import me.illuminator3.packettransfer.packet.PacketHandler;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PrePacketHandler
     extends PacketHandler
 {
-    @Override public void registerPackets() {}
+    private final Map<Integer, Class<? extends Packet>> queue = new HashMap<>();
+
+    @Override
+    public void registerPackets()
+    {
+        this.queue.forEach(this::addPacket);
+    }
 
     public void addPacket(int id, Packet packet)
     {
@@ -16,5 +25,15 @@ public class PrePacketHandler
     public void addPacket(int id, Class<? extends Packet> packet)
     {
         this.registerPacket(packet, id);
+    }
+
+    public void queuePacket(int id, Packet packet)
+    {
+        this.queuePacket(id, packet.getClass());
+    }
+
+    public void queuePacket(int id, Class<? extends Packet> packet)
+    {
+        this.queue.put(id, packet);
     }
 }
